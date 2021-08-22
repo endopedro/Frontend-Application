72e8007a221742d5096cd62bd212faca78972479
Pedro Endo <endopedro@gmail.com>
2021-08-22 16:31:57 -0300

    feat: topbar layout

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Topbar = () => {
-  return <div>topbar</div>
+  const logo = useSelector((state) => state.config.data?.logo)
+
+  return (
+    <div className="topbar">
+      <img className="logo" src={logo} alt="logo" />
+    </div>
+  )
 }
 
 export default Topbar

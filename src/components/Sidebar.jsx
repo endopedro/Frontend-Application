a13f2a5816b9892e614967edfef7ddb36bbaaa33
Pedro Endo <endopedro@gmail.com>
2021-08-22 14:24:45 -0300

    feat: sidebar navigation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Sidebar = () => {
-  return <div>sidebar</div>
+  return (
+    <div className="sidebar">
+      <div className="nav">
+        <Link href="/" className="nav-link">
+          <a>Main Page</a>
+        </Link>
+        <Link href="/product" className="nav-link">
+          <a>Product</a>
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar

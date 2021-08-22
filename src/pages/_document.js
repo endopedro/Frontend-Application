63b58def269a3083b081e339960ca9a73eacd710
Pedro Endo <endopedro@gmail.com>
2021-08-22 14:31:39 -0300

    chore: configure font

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,6 +11,12 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />

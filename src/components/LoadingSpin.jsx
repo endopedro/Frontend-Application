4cee47b00db8c9c0e2031149c514ff4e085c0ca0
Pedro Endo <endopedro@gmail.com>
2021-08-22 23:58:14 -0300

    feat: loading component

---
+++ src/components/LoadingSpin.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactLoading from 'react-loading'
+
+const LoadingSpin = () => (
+  <div className="loading-spin">
+    <ReactLoading type="spin" color="#1546b8" height={90} width={90} />
+  </div>
+)
+
+export default LoadingSpin
